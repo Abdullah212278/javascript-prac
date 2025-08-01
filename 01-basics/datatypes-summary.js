1d21fa49ca0da551be3b9354898eb1a17eb61b64
Abdullah273295 <shahsab20799@gmail.com>
2025-08-01 21:42:47 +0500

    Add new file

---
+++ 01-basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+// primirtive 
+
+
+// 7 types : string, number, boolean, null. undefined, symbol, bigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp  = null
+let userEmail;
+
+const id = Symbol('1234')
+const anotherId = Symbol('1234')
+
+// console.log(id === anotherId);
+
+const bigNumber = 8329832784329828378392728065n
+
+// reference (non primitive)
+
+// array objects functions
+
+
+const heroes = ["quaid e azam", "allama iqbal"]
+let myObj = {
+     name: "Abdullah",
+     age: 16,
+}
+
+
+const myFunction = function (){
+    console.log("Hello world");
+}
+
+console.log(typeof myFunction);
